03f8cb466cdaa9056361e8dd691222eff03449f0
Valentyn <valentynd91@gmail.com>
2019-03-14 14:58:21 +0200

    Использование миксинов

--- src/List.vue
+++ src/List.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
 
-        <h2>{{ title }}</h2>
+    <h1>List</h1>
 
         <input type="text" v-model="searchName">
 
@@ -9,13 +9,7 @@
             <li v-for="name of filteredNames">{{ name }}</li>
         </ul>
 
-
-        <hr>
-
-        <app-list></app-list>
-
     </div>
-
 </template>
 
 <script>
@@ -23,17 +17,10 @@
     import ListMixin from './listMixin'
 
     export default {
-        data() {
-            return {
-                title: 'Hello I am Vue',
-            }
-        },
-        mixins: [ListMixin],
+        mixins: [ListMixin]
     }
-
 </script>
 
 <style scoped>
-
 
 </style>
